113c5f99bfba9adaf50871635c3d6cc3754016b3
clayriell <17.denyseptian@gmail.com>
2025-09-06 21:37:29 +0700

    add getTugByMasterId and getDetail endpoint

--- controllers/assist-tug.js
+++ controllers/assist-tug.js
@@ -59,4 +59,54 @@ module.exports = {
         .json({ status: false, message: "Internal server error" });
     }
   },
-};
+  getByTugMaster : async (req , res)=>{
+    try {
+      const user = req.user
+
+      const assistTug = await prisma.assistTug.findFirst({
+        where: { masterId: user.id },
+      })
+      
+      if(!assistTug){
+        return res.status(404).json({
+          status : false , message  : "assist tug not found"
+        })
+      }
+      return res.status(200).json({
+        status : true , message : "success get assist tug detail" , data : assistTug
+      })
+    } catch (error) {
+      return res
+        .status(501)
+        .json({ status: false, message: "Internal server error" });
+    }
+    },
+    getDetail : async (req , res)=>{
+    try {
+      const user = req.params
+      const id = req.params
+
+      const assistTug = await prisma.assistTug.findUnique({
+        where: {id : Number(id)},
+      })
+      if(!assistTug){
+        return res.status(404).json({
+          status : false , message  : "assist tug not found"
+        })
+      }
+      if(assistTug.companyId !== user.companyId){
+        return res.status(403).json({
+          status : false , message : "Forbidden access, please check your company"
+        })
+      }
+      return res.status(200).json({
+        status : true , message : "success get assist tug detail" , data : assistTug
+      })
+    } catch (error) {
+      return res
+        .status(501)
+        .json({ status: false, message: "Internal server error" });
+    }
+    }
+  }
+
--- routes/assist-tug.js
+++ routes/assist-tug.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { getAll, getList, getAssistTug } = require("../controllers/assist-tug");
+const { getAll, getList, getAssistTug, getDetail, getByTugMaster } = require("../controllers/assist-tug");
 const { authenticate } = require("../middleware/auth");
 const { mustRole } = require("../middleware/role");
 const router = express.Router();
 
 router.get("/sys-all", authenticate, mustRole("SYS_ADMIN"), getAll);
 router.get("/all", authenticate, mustRole("ADMIN"), getAssistTug);
+router.get("/master-tug", authenticate, mustRole("TUG_MASTER"), getByTugMaster);
+router.get("/:id/detail", authenticate, getDetail);
 router.get("/list", getList);
 // router.get("/company", authenticate, getByCompany);
 
